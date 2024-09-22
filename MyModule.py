bdb220ad1431c59a689272a30c4c79ae47e73998
ehcouto <coutoeh@gmail.com>
2024-09-22 18:31:41 +0200

    Add files via upload

---
+++ MyModule.py
@@ -0,0 +1,15 @@
+'''
+Created on 11 de dez. de 2021
+
+@author: ehcouto
+'''
+
+print"Hello"
+
+x = 0
+while x < 5:
+    x += 1
+    print"Number Increased! - " + str(x)
+    
+    
+print"Finished!"
